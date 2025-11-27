433d8ec3b203dd9050b14521a043394c4474a977
GehanLiyanage2002 <iit22002.std.uwu.ac.lk>
2025-11-27 11:52:08 +0530

    Initial Commit

---
+++ ExpensesTracker/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ExpensesTracker.Models
+{
+    public class Category
+    {
+
+        
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public String Icon { get; set; }   = " ";
+
+        [Column(TypeName = "nvarchar(10)")]
+        public String Type { get; set; } = "Expense"; // Expense or Income
+    }
+}
---
+++ ExpensesTracker/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace ExpensesTracker.Models
+{
+    public class Transaction
+    {
+        
+        public int TransactionId { get; set; }
+        public int CategoryId { get; set; }
+        public Category categoryId { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+        public string? Note { get; set; }
+    }
+}
